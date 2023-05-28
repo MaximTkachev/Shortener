00b4fe40ea97ffbc1c9b7338342801b596aeee16
MaximTkachev <69761681+MaximTkachev@users.noreply.github.com>
2023-05-28 11:53:02 +0700

    create project

---
+++ src/main/kotlin/ru/qwerty/shortener/ShortenerApplication.kt
@@ -0,0 +1,11 @@
+package ru.qwerty.shortener
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShortenerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShortenerApplication>(*args)
+}
